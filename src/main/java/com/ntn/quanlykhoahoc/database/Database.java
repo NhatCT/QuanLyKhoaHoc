963af576a65cd8c54692dbda2af99a5539ac9b11
cammtuxinhdep <camstuscools@gmail.com>
2025-04-01 19:49:52 +0700

    Test lại các testcase fail, kiểm thử tính năng Quên mật khẩu

--- src/main/java/com/ntn/quanlykhoahoc/database/Database.java
+++ src/main/java/com/ntn/quanlykhoahoc/database/Database.java
@@ -26,7 +26,7 @@ public class Database {
     }
     
     public static Connection getConn() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost/quanlykhoahoc", "root", "Nhat#1908");
+        return DriverManager.getConnection("jdbc:mysql://localhost/quanlykhoahoc", "root", "Abc123");
     }
 
 
